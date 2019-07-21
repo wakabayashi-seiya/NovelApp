d44bdbafcbfcfafa2518c55743158295a157a642
Seiya <e1517234@soka-u.jp>
2019-07-21 05:39:11 +0000

    update: 画像アップロード先をAWSのS3にしました

--- Gemfile
+++ Gemfile
@@ -40,7 +40,8 @@ end
 
 group :production do
   gem 'pg', '0.20.0'
+  gem 'fog', '1.42'
 end
 
-# Windows環境ではtzinfo-dataというgemを含める必要があります
+
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,8 +6,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [150, 200]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
