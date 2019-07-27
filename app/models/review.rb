0be2f8c2ff745c62febfae077912eaabfda5afcb
Seiya <e1517234@soka-u.jp>
2019-07-27 05:40:25 +0000

    update: WYSIWYG Editorとしてsummernoteを導入、form_forの代わりにsimple_formを導入、bootstrap3からbootstrap4に移行、その他レイアウトを整えました

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,8 @@ class Review < ApplicationRecord
   belongs_to :novel
   belongs_to :user
   
+  validates :title, presence: true
+  validates :text, presence: true
+  
   default_scope -> { order(created_at: :desc) }
 end
