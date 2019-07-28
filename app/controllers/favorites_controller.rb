adbbf165944e48777a814217e8a491099deacf02
Seiya <e1517234@soka-u.jp>
2019-07-28 05:31:29 +0000

    update: レイアウトを整えました

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,19 +3,19 @@ class FavoritesController < ApplicationController
     @novel = Novel.find(params[:novel_id])
     current_user.like(@novel)
     redirect_back(fallback_location: root_path) 
-    # respond_to do |format|
-    #   format.html { redirect_back(fallback_location: root_path) } 
-    #   format.js
-    # end
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) } 
+      format.js
+    end
   end
 
   def destroy
     @novel = Novel.find(params[:novel_id])
     current_user.unlike(@novel)
     redirect_back(fallback_location: root_path) 
-    # respond_to do |format|
-    #   format.html { redirect_back(fallback_location: root_path) }
-    #   format.js
-    # end
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) }
+      format.js
+    end
   end
 end
