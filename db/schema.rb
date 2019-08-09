7e6552009f7cfc8ba0dcd044e6a3239e00e300fb
Seiya <e1517234@soka-u.jp>
2019-08-09 14:50:33 +0000

    update: rails_adminを導入して管理画面を実装しました。また、omniauth-twitterを使って、Twitter認証機能を実装しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190805023244) do
+ActiveRecord::Schema.define(version: 20190808031807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,12 +100,11 @@ ActiveRecord::Schema.define(version: 20190805023244) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.boolean "admin"
     t.string "field"
     t.string "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "lastaccesstime", default: "2019-08-06 14:34:49"
+    t.datetime "lastaccesstime", default: "2019-08-09 14:40:10"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -122,6 +121,10 @@ ActiveRecord::Schema.define(version: 20190805023244) do
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
+    t.boolean "admin", default: false
+    t.string "provider"
+    t.string "uid"
+    t.string "username"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
