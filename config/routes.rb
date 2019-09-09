b0dad4adea7ca252951f5d3332ad7f9d7ff220ba
Seiya <e1517234@soka-u.jp>
2019-09-09 03:23:14 +0000

    fix: Twitter認証を修正しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
+  devise_for :users, controllers: { :omniauth_callbacks => "users/omniauth_callbacks" }
   root 'static_pages#home'
   get '/help', to: "static_pages#help"
   get '/about', to: "static_pages#about"
