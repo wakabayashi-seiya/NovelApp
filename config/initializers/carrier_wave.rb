abf6b2700175a38f25e21f1b9c0d75acc4276708
Seiya <e1517234@soka-u.jp>
2019-09-13 14:48:05 +0000

    fix: 画像アップロードのため、ファイルを修正しました

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,12 +2,12 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = ENV['S3_BUCKET']
+    config.fog_directory  = 'novelapp'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['S3_ACCESS_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET_KEY'],
-      region: ENV['S3_REGION'],
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_REGION'],
       path_style: true
     }
   else
@@ -17,7 +17,6 @@ CarrierWave.configure do |config|
 end
  
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
-
 # if Rails.env.production?
 #   CarrierWave.configure do |config|
 #     config.fog_credentials = {
