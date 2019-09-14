7a45c3b52eb07bdd5fbc0431d9836130be774d12
Seiya <e1517234@soka-u.jp>
2019-09-14 03:39:26 +0000

    fix: 画像アップロードのためのファイルを修正しました

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -52,10 +52,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-  
-  protected
-  def secure_token(length=16)
-    var = :"@#{mounted_as}_secure_token"
-    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
-  end
 end
